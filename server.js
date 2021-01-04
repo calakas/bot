73ab615f0183f3f3fc1eb63d89dbb6cf1c97251c
calakas <75955169+calakas@users.noreply.github.com>
2021-01-04 13:58:18 -0500

    Update server.js

--- server.js
+++ server.js
@@ -141,14 +141,14 @@ client.on("message", message => {
   if (message.content === prefix + "avatar") {
     const avatar = new MessageEmbed()
     .setTitle("Avatar")
-.setDescription(`[Descargar Avatar](${user.displayAvatarURL({
+.setDescription(`[Descargar Avatar]` + user.displayAvatarURL({
         format: 'png',
         dynamic: true
-    })})`)
+    }))
 .setImage(user.displayAvatarURL({dynamic: true, size : 1024 }))
 .setColor("RANDOM")
-.setFooter(`Avatar de solicitado por: ${msg.member.displayName}`);
-msg.channel.send(avatar)
+.setFooter(`Avatar de solicitado por` + message.member.displayName);
+message.channel.send(avatar);
   }      
 
   if (message.content === prefix + "help") {
