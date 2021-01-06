1c4735c1641d5c11171526fff83aecebd0280e91
calakas <75955169+calakas@users.noreply.github.com>
2021-01-06 12:11:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -128,10 +128,7 @@ client.on("message", message => {
 if(message.content === prefix + "llorar") {
 const embed = new MessageEmbed()
    .setTitle(message.member.displayName, "esta llorando")
-   .setDescription(`[URL](${https://media.tenor.com/images/6abfd1c8889e0c5a2db0e1039d499c9f/tenor.gif({
-        format: 'png',
-        dynamic: true
-    })})`)
+   .setDescription(`[URL](https://media.tenor.com/images/6abfd1c8889e0c5a2db0e1039d499c9f/tenor.gif)`)
    .setImage(https://media.tenor.com/images/6abfd1c8889e0c5a2db0e1039d499c9f/tenor.gif)
 
    .setColor("RANDOM")
